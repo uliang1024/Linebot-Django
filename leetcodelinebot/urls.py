60cbd3d3f3147a307a6c2fdc0f5473f7a08801bb
uliang <amisleo000@gmail.com>
2023-06-25 01:10:09 +0800

    連結line bot

--- leetcodelinebot/urls.py
+++ leetcodelinebot/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
- 
+
+app_name = 'leetcodelinebot'
+
 urlpatterns = [
-    path('callback', views.callback)
+    path('callback/', views.callback, name='callback'),
 ]
