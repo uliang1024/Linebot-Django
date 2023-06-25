bd50547b849898b31e2bc3d99eefc471c6a033e1
uliang <amisleo000@gmail.com>
2023-06-25 18:56:07 +0800

    讀取資料

--- leetcodelinebot/models.py
+++ leetcodelinebot/models.py
@@ -1,8 +1,5 @@
 from django_mongoengine import Document, fields
 
 class ReportLog(Document):
-    _id = fields.StringField(max_length=255)
     name = fields.StringField(max_length=255)
     Done = fields.BooleanField()
-    _created_at = fields.DateTimeField()
-    _updated_at = fields.DateTimeField()
--- leetcodelinebot/views.py
+++ leetcodelinebot/views.py
@@ -8,6 +8,8 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 
+from leetcodelinebot.models import ReportLog
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
@@ -27,10 +29,26 @@ def callback(request):
  
         for event in events:
             if isinstance(event, MessageEvent):  # 如果有訊息事件
-                line_bot_api.reply_message(  # 回復傳入的訊息文字
-                    event.reply_token,
-                    TextSendMessage(text=event.message.text)
-                )
+                if event.message.text == '資料':
+                    # 查询 ReportLog 文档的数据
+                    report_logs = ReportLog.objects.all()
+
+                    # 遍历查询结果
+                    for report_log in report_logs:
+                        name = report_log.name
+                        done = report_log.Done
+
+                        # 将 name 和 Done 回复给用户
+                        reply_text = f"name: {name}, Done: {done}"
+                        line_bot_api.reply_message(
+                            event.reply_token,
+                            TextSendMessage(text=reply_text)
+                        )
+                else:
+                    line_bot_api.reply_message(  # 回復傳入的訊息文字
+                        event.reply_token,
+                        TextSendMessage(text=event.message.text)
+                    )
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
