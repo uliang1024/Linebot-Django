5c4e8a49bf049d65724cd04d9d17774222f41f22
uliang <amisleo000@gmail.com>
2023-06-25 17:53:57 +0800

    讀取資料表

--- leetcodelinebot/models.py
+++ leetcodelinebot/models.py
@@ -1,1 +1,8 @@
-from django.db import models
+from django_mongoengine import Document, fields
+
+class ReportLog(Document):
+    _id = fields.StringField(max_length=255)
+    name = fields.StringField(max_length=255)
+    Done = fields.BooleanField()
+    _created_at = fields.DateTimeField()
+    _updated_at = fields.DateTimeField()
