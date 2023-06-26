ffa96054cb2be74419a8aaeed5f3855c52a05180
uliang <amisleo000@gmail.com>
2023-06-27 03:02:47 +0800

    設定鬧鐘提醒

--- leetcodelinebot/views.py
+++ leetcodelinebot/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
- 
+
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
@@ -12,6 +12,8 @@ from datetime import datetime, timedelta
 
 import pytz
 import re
+import schedule
+import time
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
@@ -50,7 +52,7 @@ def callback(request):
                     else:
                         line_bot_api.reply_message(
                             event.reply_token,
-                            TextSendMessage(text='未提取到數字部分，舉例：[完成 1]')  # 回覆未提取到數字訊息
+                            TextSendMessage(text='未提取到數字，舉例:[完成 1]')  # 回覆未提取到數字訊息
                         )
                     
         return HttpResponse()
@@ -88,3 +90,71 @@ def extract_topic_from_message(message):
     
     # 若未提取到數字部分，回傳 None
     return None
+
+def get_report_24_hours(user_id):
+    taiwan_tz = pytz.timezone('Asia/Taipei')
+    now = datetime.now(taiwan_tz)
+    past_24_hours = now - timedelta(hours=24)
+
+    past_24_hours_topics = ReportLog.objects(name=user_id, created_at__gte=past_24_hours).distinct("topic")
+    past_24_hours_count = len(past_24_hours_topics)
+
+    return past_24_hours_count
+
+def remind_user():
+    # 早上9點提醒，顯示過去24小時中每位使用者完成的題目總數
+    # 若有使用者未達到2次，則在使用者名稱後面加上⚠️
+    # 呼叫相應的函式來取得數據並組織回覆訊息
+    # 使用 line_bot_api 調用 Line Bot API 來發送提醒訊息
+    
+    # 獲取所有使用者的名稱
+    users = ReportLog.objects.distinct("name")
+    
+    reply_text = "過去24小時中：\n"
+    
+    for user in users:
+        user_id = user.name
+        count = get_report_24_hours(user_id)
+        reply_text += f"{user_id}已完成{count}題"
+        if count < 2:
+            reply_text += " ⚠️"
+        reply_text += "\n"
+    
+    # 發送提醒訊息給所有使用者
+    for user in users:
+        user_id = user.name
+        line_bot_api.push_message(user_id, TextSendMessage(text=reply_text))
+
+def remind_user_afternoon():
+    # 下午3點提醒使用者請記得做 LeetCode 題目
+    # 使用 line_bot_api 調用 Line Bot API 來發送提醒訊息
+    
+    # 獲取所有使用者的名稱
+    users = ReportLog.objects.distinct("name")
+    
+    for user in users:
+        user_id = user.name
+        # 發送提醒訊息給使用者
+        line_bot_api.push_message(user_id, TextSendMessage(text="請記得做 LeetCode 題目"))
+
+def remind_user_evening():
+    # 晚上9點提醒使用者請記得回報今日完成的題目
+    # 使用 line_bot_api 調用 Line Bot API 來發送提醒訊息
+    
+    # 獲取所有使用者的名稱
+    users = ReportLog.objects.distinct("name")
+    
+    for user in users:
+        user_id = user.name
+        # 發送提醒訊息給使用者
+        line_bot_api.push_message(user_id, TextSendMessage(text="請記得回報今日完成的題目"))
+
+# 設定排程任務
+schedule.every().day.at("03:05").do(remind_user)
+schedule.every().day.at("15:00").do(remind_user_afternoon)
+schedule.every().day.at("21:00").do(remind_user_evening)
+
+# 進入無限迴圈，持續執行排程任務
+while True:
+    schedule.run_pending()
+    time.sleep(1)
