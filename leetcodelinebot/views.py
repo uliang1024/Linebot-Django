635589f8a6713b3f159b08f52401a1b66834c1ab
uliang <amisleo000@gmail.com>
2023-07-05 15:38:22 +0800

    壞掉了啦

--- leetcodelinebot/views.py
+++ leetcodelinebot/views.py
@@ -9,11 +9,6 @@ from linebot.models import MessageEvent, TextSendMessage
 
 from leetcodelinebot.models import write_to_report_log, get_report_stats, send_line_message, get_past_24_hours_stats, extract_topic_from_message
 
-import schedule
-import time
-from datetime import datetime
-import pytz
-
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
