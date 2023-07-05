015a9eccca9836396164cd8760d45d7a0334f5cd
uliang <amisleo000@gmail.com>
2023-07-05 20:32:29 +0800

    測試

--- leetcodelinebot/views.py
+++ leetcodelinebot/views.py
@@ -30,59 +30,40 @@ def callback(request):
             return HttpResponseBadRequest()
  
         for event in events:
-            if isinstance(event, JoinEvent):  # 如果有加入聊天室的事件
-                # group_id = event.source.group_id  # 群組ID
-                # user_ids = line_bot_api.get_group_member_ids(group_id)  # 取得群組內使用者ID列表
-                # # 將每個使用者ID新增至MongoDB的UsersCollection
-                # for user_id in user_ids:
-                #     profile = line_bot_api.get_profile(user_id)
-                #     user = Users(
-                #         user_id = user_id,
-                #         display_name = profile.display_name,
-                #         status_message = profile.status_message,
-                #         picture_url = profile.picture_url,
-                #         # 其他使用者相關的欄位值
-                #     )
-                #     user.save()  # 將使用者物件保存至MongoDB的UsersCollection
+            # if isinstance(event, FollowEvent):  # 如果是加好友事件
+            #     user_id = event.source.user_id
+            #     profile = line_bot_api.get_profile(user_id)
 
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='大家好，我是Line bot！\n請將我加為好友才能為你服務！')  # 聊天室歡迎訊息
-                )
-            elif isinstance(event, FollowEvent):  # 如果是加好友事件
-                # user_id = event.source.user_id
-                # profile = line_bot_api.get_profile(user_id)
-
-                # # 更新使用者資訊
-                # user = Users.objects(user_id=user_id).first()
-                # if user:
-                #     user.display_name = profile.display_name
-                #     user.status_message = profile.status_message
-                #     user.picture_url = profile.picture_url
-                #     user.save()
+            #     # 更新使用者資訊
+            #     user = Users.objects(user_id=user_id).first()
+            #     if user:
+            #         user.display_name = profile.display_name
+            #         user.status_message = profile.status_message
+            #         user.picture_url = profile.picture_url
+            #         user.save()
                 
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='有人偷偷加我好友')  # 聊天室歡迎訊息
-                )
-            elif isinstance(event, MemberJoinedEvent):  # 如果是新的使用者加入群組事件
-                # user_id = event.joined.members[0].user_id  # 取得新加入使用者的 ID
-                # profile = line_bot_api.get_profile(user_id)
+            #     line_bot_api.reply_message(
+            #         event.reply_token,
+            #         TextSendMessage(text='有人偷偷加我好友')  # 聊天室歡迎訊息
+            #     )
+            # elif isinstance(event, MemberJoinedEvent):  # 如果是新的使用者加入群組事件
+            #     user_id = event.joined.members[0].user_id  # 取得新加入使用者的 ID
+            #     profile = line_bot_api.get_profile(user_id)
 
-                # # 儲存使用者資訊到資料庫
-                # user = Users(
-                #     user_id=user_id,
-                #     display_name=profile.display_name,
-                #     status_message=profile.status_message,
-                #     picture_url=profile.picture_url,
-                #     punish=0
-                # )
-                # user.save()
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='歡迎歡迎新朋友')  # 聊天室歡迎訊息
-                )
-            elif isinstance(event, MessageEvent):  # 如果有訊息事件
+            #     # 儲存使用者資訊到資料庫
+            #     user = Users(
+            #         user_id=user_id,
+            #         display_name=profile.display_name,
+            #         status_message=profile.status_message,
+            #         picture_url=profile.picture_url,
+            #         punish=0
+            #     )
+            #     user.save()
+            #     line_bot_api.reply_message(
+            #         event.reply_token,
+            #         TextSendMessage(text='歡迎歡迎新朋友')  # 聊天室歡迎訊息
+            #     )
+            if isinstance(event, MessageEvent):  # 如果有訊息事件
                 if event.message.text == '查詢紀錄':
                     user_id = event.source.user_id
                     reply_text = get_report_stats(user_id)  # 呼叫函式取得 ReportLog 統計數據
@@ -109,6 +90,25 @@ def callback(request):
                 elif event.message.text == '測試':
                     text = get_past_24_hours_stats() 
                     send_line_message(text)
+                elif event.message.text == '新增':
+                    group_id = event.source.group_id  # 群組ID
+                    user_ids = line_bot_api.get_group_member_ids(group_id)  # 取得群組內使用者ID列表
+                    # 將每個使用者ID新增至MongoDB的UsersCollection
+                    for user_id in user_ids:
+                        profile = line_bot_api.get_profile(user_id)
+                        user = Users(
+                            user_id = user_id,
+                            display_name = profile.display_name,
+                            status_message = profile.status_message,
+                            picture_url = profile.picture_url,
+                            # 其他使用者相關的欄位值
+                        )
+                        user.save()  # 將使用者物件保存至MongoDB的UsersCollection
+
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text='大家好，我是Line bot！\n請將我加為好友才能為你服務！')  # 聊天室歡迎訊息
+                    )
                     
         return HttpResponse()
     else:
