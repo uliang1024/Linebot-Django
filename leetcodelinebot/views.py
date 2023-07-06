63866b9418a171ebedf2214bafad564fb3b1a6ee
uliang <amisleo000@gmail.com>
2023-07-06 11:59:41 +0800

    測試

--- leetcodelinebot/views.py
+++ leetcodelinebot/views.py
@@ -214,9 +214,9 @@ scheduler = BlockingScheduler()
 taipei_tz = timezone('Asia/Taipei')
 
 # 添加定时任务，并设置触发时间（台湾时间）
-scheduler.add_job(settlement_event, 'cron', hour=11, minute=53, timezone=taipei_tz)
-scheduler.add_job(reminder_event, 'cron', hour=11, minute=54, timezone=taipei_tz)
-scheduler.add_job(report_event, 'cron', hour=11, minute=55, timezone=taipei_tz)
+scheduler.add_job(settlement_event, 'cron', hour=12, minute=2, timezone=taipei_tz)
+scheduler.add_job(reminder_event, 'cron', hour=12, minute=4, timezone=taipei_tz)
+scheduler.add_job(report_event, 'cron', hour=12, minute=6, timezone=taipei_tz)
 
 # 启动调度器
 scheduler.start()
