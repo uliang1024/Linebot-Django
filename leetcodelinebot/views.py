1865d4c8f4fd18b18a926f1189d6c9b8ed0b1733
uliang <amisleo000@gmail.com>
2023-07-06 11:38:56 +0800

    修正

--- leetcodelinebot/views.py
+++ leetcodelinebot/views.py
@@ -7,7 +7,7 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage, JoinEvent, FollowEvent, MemberJoinedEvent
 
-from leetcodelinebot.models import Users, ReportLog, write_to_report_log, get_report_stats, extract_topic_from_message, settlement_event, reminder_event, report_event
+from leetcodelinebot.models import Users, ReportLog, write_to_report_log, get_report_stats, extract_topic_from_message
 
 from datetime import datetime, timedelta
 from pytz import timezone
